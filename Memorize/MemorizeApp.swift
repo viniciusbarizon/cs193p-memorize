fb0566be0efe104dc7bb18fef12c8c1cf6781ce6
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-09-05 11:44:11 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Vinicius Barizon on 05/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
