d7f4b109aa827fe2606b00778eb42df1af825752
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-09-07 14:17:01 +0100

    CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            CardView(IsFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var IsFaceUp: Bool = false
+
+    var body: some View {
+        ZStack(content: {
+            if IsFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }
+            else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+
+        })
     }
 }
 
